1e90c8b371ceb50d69feb2609e1c9b7d72947870
juankhusuma <66712975+juankhusuma@users.noreply.github.com>
2024-04-15 18:23:26 +0700

    Implement receive_notification function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -88,4 +88,9 @@ impl NotificationService {
             .join()
             .unwrap();
     }
+
+    pub fn recieve_notification(payload: Notification) -> Result<Notification> {
+        let subscriber_result = NotificationRepository::add(payload.clone());
+        return Ok(subscriber_result);
+    }
 }
